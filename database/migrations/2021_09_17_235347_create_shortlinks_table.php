534e28de283e8984a1cd80e5972c0e059f974356
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-09-18 08:23:34 -0400

    Shortlinks table created
    shortlinks table created with fields code, url , nsfw and visits

---
+++ database/migrations/2021_09_17_235347_create_shortlinks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShortlinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shortlinks', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('url');
+            $table->boolean('nsfw')->default(false);
+            $table->unsignedInteger('visits')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shortlinks');
+    }
+}
