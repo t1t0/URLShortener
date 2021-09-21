4b7566f2b5c54fbad8a18b359d6b434f09289013
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-09-20 23:45:18 -0400

    Update welcome.blade.php
    New livewire components added to the main view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="uk-grid-collapse" uk-grid>
-    <div class="uk-width-1-1 uk-width-large-1-4">
-        
+<div class="uk-grid-collapse uk-flex-center uk-flex-middle" uk-grid style="flex-grow: 1">
+    <div class="uk-width-1-1 uk-width-1-4@l">
+        @livewire('shortlink-form')
+        @livewire('shortlink-card')
     </div>
 </div>
 @endsection
