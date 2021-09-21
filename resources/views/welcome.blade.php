39015b82f09a7e3249604bbbb4cd060d0cf80c03
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-09-20 23:47:05 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="uk-grid-collapse uk-flex-center uk-flex-middle" uk-grid style="flex-grow: 1">
-    <div class="uk-width-1-1 uk-width-1-4@l">
+    <div class="uk-width-1-1 uk-width-1-3@l">
         @livewire('shortlink-form')
         @livewire('shortlink-card')
     </div>
